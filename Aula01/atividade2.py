e86422f9e98a4d7b557a1695adc84e8679ecc15e
diogommello@gmail.com <51134552024.01@senacrjedu.local>
2024-09-19 19:09:51 -0300

    diretorio da aula01

---
+++ Aula01/atividade2.py
@@ -0,0 +1,22 @@
+
+# num1 = float(input('Escreva um numero :'))
+# ant = (num1-1) 
+# suc = (num1+1)
+
+# print(f'O numero escolhido foi {num1}, seu antecessor e {ant} e seu sucessor e {suc}')
+
+
+# num2 = float(input('Escreva um numero :'))
+# dobro = (num2*2) 
+# triplo = (num2*3)
+# quad = (num2*num2)
+
+# print(f'O numero escolhido foi {num2}, seu dobro e {dobro}, seu triplo e {triplo} e seu quadrado e {quad}')
+
+
+teste = float(input('Escreva a nota do teste :'))
+prova = float(input('Escreva a nota da prova :'))
+
+media = ((teste+prova)/2)
+
+print(f'Sua media foi {media}')
